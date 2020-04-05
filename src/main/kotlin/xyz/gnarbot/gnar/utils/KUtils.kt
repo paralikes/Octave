6d80a0f5a61013745590554e8d9714362300951e
Devoxin <lukel9914@gmail.com>
2020-04-05 21:26:10 +0100

    thicc time parser

--- src/main/kotlin/xyz/gnarbot/gnar/utils/KUtils.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/utils/KUtils.kt
@@ -58,17 +58,20 @@ fun parseDuration(input: String): Long {
 
 }
 
-fun getDisplayValue(ms: Long): String {
-    val duration = Duration.ofMillis(ms)
-    val hours = duration.toHours();
-    val minutes = duration.minusHours(hours).toMinutes()
-    val seconds = duration.minusMinutes(minutes).seconds
+fun getDisplayValue(ms: Long, shorthand: Boolean = false): String {
+    val seconds = ms / 1000 % 60
+    val minutes = ms / (1000 * 60) % 60
+    val hours = ms / (1000 * 60 * 60) % 24
+    val days = ms / (1000 * 60 * 60 * 24)
 
     return when {
-        hours > 0 -> "$hours hours, $minutes minutes and $seconds seconds"
-        minutes > 0 -> "$minutes minutes and $seconds seconds"
-        seconds > 0 -> "$seconds seconds"
-        else -> "$hours hours, $minutes minutes and $seconds seconds"
+        shorthand && days > 0 -> String.format("%02d:%02d:%02d:%02d", days, hours, minutes, seconds)
+        shorthand && hours > 0 -> String.format("%02d:%02d:%02d", hours, minutes, seconds)
+        shorthand -> String.format("%02d:%02d", minutes, seconds)
+        days > 0 -> String.format("%d days, %d hours, %d minutes and %d seconds", days, hours, minutes, seconds)
+        hours > 0 -> String.format("%d hours, %d minutes and %d seconds", hours, minutes, seconds)
+        minutes > 0 -> String.format("%d minutes and %d seconds", minutes, seconds)
+        else -> String.format("%d seconds", seconds)
     }
 }
 
