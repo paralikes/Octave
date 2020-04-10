1ef596977f7faf0eb990bfe4eb78970d815b4c37
kodehawa <david.alejandro.rubio@gmail.com>
2020-04-10 12:54:08 -0400

    Remember to open an audio connection on queue resume lol

--- src/main/kotlin/xyz/gnarbot/gnar/commands/music/ResumeCommand.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/commands/music/ResumeCommand.kt
@@ -17,17 +17,23 @@ class ResumeCommand : CommandExecutor() {
         val manager = context.bot.players.get(context.guild)
         val scheduler = manager.scheduler
 
+        if(context.voiceChannel == null) {
+            context.send().error("You need to be in a voice channel.").queue()
+            return
+        }
+
         if (scheduler.queue.isEmpty()) {
             context.send().issue("The queue is empty.\n$PLAY_MESSAGE").queue()
             return
         }
 
         if (scheduler.lastTrack != null) {
-            context.send().error("There's nothing to resume as the player has been active here!")
+            context.send().error("There's nothing to resume as the player has been active here!").queue()
             return
         }
 
         //Poll next from queue and force that track to play.
+        manager.openAudioConnection(context.voiceChannel, context)
         scheduler.nextTrack()
 
         context.send().info("Queue has been resumed.").queue()
