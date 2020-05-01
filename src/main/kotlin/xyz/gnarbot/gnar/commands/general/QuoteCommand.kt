2ea25bd10d3fffde957501cf368f054a5ffd5af9
Devoxin <lukel9914@gmail.com>
2020-05-01 15:22:04 +0100

    Cleanup pt4 [nobuild]

--- src/main/kotlin/xyz/gnarbot/gnar/commands/general/QuoteCommand.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/commands/general/QuoteCommand.kt
@@ -1,6 +1,7 @@
 package xyz.gnarbot.gnar.commands.general
 
 import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.exceptions.ErrorResponseException
 import xyz.gnarbot.gnar.commands.BotInfo
 import xyz.gnarbot.gnar.commands.Command
 import xyz.gnarbot.gnar.commands.CommandExecutor
@@ -17,32 +18,31 @@ class QuoteCommand : CommandExecutor() {
     private val fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
     override fun execute(context: Context, label: String, args: Array<String>) {
         if (args.isEmpty()) {
-            context.bot.commandDispatcher.sendHelp(context, info)
-            return
+            return context.bot.commandDispatcher.sendHelp(context, info)
         }
 
-        val targetChannel = when {
-            context.message.mentionedChannels.size > 0 -> context.message.mentionedChannels[0]
-            else -> context.message.textChannel
+        val targetChannel = context.message.mentionedChannels[0] ?: context.textChannel
+        val messageIds = args.filter { it != targetChannel.asMention }.map { it.toLongOrNull()?.toString() }
+        val invalid = messageIds.filter { it == null }
+
+        if (invalid.isNotEmpty()) {
+            return context.send().error("The following arguments are invalid:\n" +
+                    invalid.joinToString("`, `", prefix = "`", postfix = "`")).queue()
         }
 
-        for (id in args) {
-            try {
-                if (id == targetChannel.asMention)
-                    continue
-
-                context.message.channel.retrieveMessageById(id).queue({
-                    targetChannel.sendMessage(EmbedBuilder()
-                            .setAuthor(it.author.name, null, it.author.avatarUrl)
-                            .setDescription(it.contentDisplay)
-                            .addField("Sent At", fmt.format(it.timeCreated), false)
-                            .setFooter("ID: ${it.id}")
-                            .build()
-                    ).queue()
-                }) { context.send().error("Invalid message ID `$id`.").queue() }
-            } catch (e: IllegalArgumentException) {
-                context.send().error("Invalid message ID `$id`.").queue()
-            }
+        for (messageId in args) {
+            context.message.channel.retrieveMessageById(messageId).queue({
+                targetChannel.sendMessage(EmbedBuilder()
+                        .setAuthor(it.author.name, null, it.author.avatarUrl)
+                        .setDescription(it.contentDisplay)
+                        .addField("Sent At", fmt.format(it.timeCreated), false)
+                        .setFooter("ID: ${it.id}")
+                        .build()
+                ).queue()
+            }, {
+                val message = if (it is ErrorResponseException) it.meaning else it.localizedMessage
+                context.send().error(message).queue()
+            })
         }
 
         context.send().info("Sent quotes to the ${targetChannel.name} channel!").queue()
