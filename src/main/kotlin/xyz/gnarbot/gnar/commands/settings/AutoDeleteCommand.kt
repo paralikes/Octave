c23a9414c20e972f9babf8a1f2e0dbeb47cc83b3
Devoxin <lukel9914@gmail.com>
2020-05-01 15:12:40 +0100

    Cleanup Kode's cleanup pt2 [nobuild]

--- src/main/kotlin/xyz/gnarbot/gnar/commands/settings/AutoDeleteCommand.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/commands/settings/AutoDeleteCommand.kt
@@ -12,16 +12,13 @@ import xyz.gnarbot.gnar.commands.*
 )
 class AutoDeleteCommand : CommandExecutor() {
     override fun execute(context: Context, label: String, args: Array<String>) {
-        val message: String
-        if (context.data.command.isAutoDelete) {
-            context.data.command.isAutoDelete = false
-            context.data.save()
-            message = "The bot will no longer automatically delete messages after 10 seconds."
-        } else {
-            context.data.command.isAutoDelete = true
-            context.data.save()
-            message = "The bot will now delete messages after 10 seconds."
-        }
-        context.send().info(message).queue()
+        val invertAutoDelete = !context.data.command.isAutoDelete
+        context.data.command.isAutoDelete = invertAutoDelete
+        context.data.save()
+
+        val send = if (invertAutoDelete) "The bot will no longer automatically delete messages after 10 seconds."
+        else "The bot will now delete messages after 10 seconds."
+
+        context.send().info(send).queue()
     }
 }
