5db4936ebef89c7140385d34bbc091dfa9491593
Devoxin <lukel9914@gmail.com>
2020-05-02 01:27:16 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/main/java/xyz/gnarbot/gnar/commands/template/annotations/Description.java
+++
@@ -1,9 +0,0 @@
-package xyz.gnarbot.gnar.commands.template.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Description {
-    String value();
-}
