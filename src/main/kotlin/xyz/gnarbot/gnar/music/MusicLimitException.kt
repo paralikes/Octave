0f315e93dc118931b06e10c487ab7f5f3596bf58
Devoxin <lukel9914@gmail.com>
2020-05-02 23:27:45 +0100

    467 errors

--- src/main/kotlin/xyz/gnarbot/gnar/music/MusicLimitException.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/music/MusicLimitException.kt
@@ -1,26 +1,29 @@
 package xyz.gnarbot.gnar.music
 
-import xyz.gnarbot.gnar.commands.Context
+import me.devoxin.flight.api.Context
 import java.awt.Color
 
 class MusicLimitException : Exception() {
-    fun sendToContext(context: Context) {
-        context.send().embed("Maximum Capacity") {
-            color { Color.ORANGE }
-            description {
+    fun sendToContext(ctx: Context) {
+        ctx.send {
+            setColor(Color.ORANGE)
+            setTitle("Maximum Capacity")
+            setDescription(
                 buildString {
                     append("Music is currently at maximum capacity, please try again later.\n")
                     append("Please consider donating to our **[Patreon](https://www.patreon.com/octavebot)** ")
                     append("to help us with hosting costs.")
                 }
-            }
-            field("Why am I seeing this?") {
+            )
+            addField(
+                "Why am I seeing this?",
                 buildString {
                     append("Our music feature has a limit on how many channels we can play to at once, so we can ")
                     append("keep our music quality high and our server healthy. Donating will allow you to ")
                     append("bypass the limit, help us expand and upgrade our processing power.")
-                }
-            }
-        }.action().queue()
+                },
+                false
+            )
+        }
     }
 }
