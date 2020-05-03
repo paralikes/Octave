a2263fa4fa4134779700f68e4c1cf8550424aa36
Devoxin <lukel9914@gmail.com>
2020-05-03 01:15:39 +0100

    karen

---
+++ src/main/kotlin/xyz/gnarbot/gnar/commands/music/dj/Stop.kt
@@ -0,0 +1,24 @@
+package xyz.gnarbot.gnar.commands.music.dj
+
+import me.devoxin.flight.api.Context
+import me.devoxin.flight.api.annotations.Command
+import xyz.gnarbot.gnar.Launcher
+import xyz.gnarbot.gnar.entities.framework.MusicCog
+import xyz.gnarbot.gnar.utils.extensions.manager
+
+class Stop : MusicCog(false, false, false) {
+    @Command(aliases = ["leave", "end", "st", "fuckoff"], description = "Stop and clear the music player.")
+    fun stop(ctx: Context, clear: Boolean = false) {
+        val karen = ctx.manager
+
+        if (clear) {
+            karen.scheduler.queue.clear()
+        }
+
+        karen.discordFMTrack = null
+        ctx.guild!!.audioManager.closeAudioConnection()
+        Launcher.players.destroy(ctx.guild!!.idLong)
+
+        ctx.send("Playback has been completely stopped. If you want to clear the queue run `${ctx.trigger}clearqueue` or `${ctx.trigger}stop yes`")
+    }
+}
