3dac8f46150847e86c6657fb37353f69023d5c80
Devoxin <lukel9914@gmail.com>
2020-05-13 22:57:59 +0100

    [restartshards] Fix todo.

--- src/main/kotlin/gg/octave/bot/commands/admin/RestartShards.kt
+++ src/main/kotlin/gg/octave/bot/commands/admin/RestartShards.kt
@@ -2,43 +2,33 @@ package gg.octave.bot.commands.admin
 
 import me.devoxin.flight.api.Context
 import me.devoxin.flight.api.annotations.Command
+import me.devoxin.flight.api.annotations.SubCommand
 import me.devoxin.flight.api.entities.Cog
 import net.dv8tion.jda.api.JDA
 
 class RestartShards : Cog {
-    @Command(description = "Restart bot shards (all/dead/#)", developerOnly = true)
-    fun restartshards(ctx: Context, filter: String) {
-        when (filter) {
-            "all" -> {
-                ctx.send("Rebooting all **${ctx.jda.shardInfo.shardTotal}** shards...")
-                ctx.jda.shardManager?.restart()
-            }
-            "dead" -> {
-                val deadShards = ctx.jda.shardManager?.shards?.filter { it.status != JDA.Status.CONNECTED }
-
-                if (deadShards == null || deadShards.isEmpty()) {
-                    return ctx.send("There are no dead shards.")
-                }
-
-                deadShards.forEach { it.shardManager?.restart(it.shardInfo.shardId) }
-                ctx.send("Queued **${deadShards.size}** shards for reboot.")
-            }
-            else -> {
-                if (filter.all { it.isDigit() }) {
-                    val num = filter.toInt()
-
-                    if (num < 0 || num >= ctx.jda.shardInfo.shardTotal) {
-                        return ctx.send("**$num** should be equal to or higher than 0, and less than ${ctx.jda.shardInfo.shardTotal}.")
-                    }
-
-                    ctx.jda.shardManager?.restart(num)
-                    return ctx.send("Rebooting shard **$filter**...")
-                }
-
-                ctx.send("c'mon man you know the rules. Gimme `all`, `dead` or `#`.")
-            }
+    @Command(aliases = ["rs"], description = "Restart bot shards.", developerOnly = true)
+    fun restartshards(ctx: Context, shardId: Int) {
+        if (shardId < 0 || shardId >= ctx.jda.shardInfo.shardTotal) {
+            return ctx.send("**$shardId** should be equal to or higher than 0, and less than ${ctx.jda.shardInfo.shardTotal}.")
         }
+
+        ctx.jda.shardManager?.restart(shardId)
+        ctx.send("Rebooting shard **$shardId**...")
     }
 
-    // TODO: Subcommands
+    @SubCommand(description = "Restart any shards that aren't connected.")
+    fun dead(ctx: Context) {
+        val deadShards = ctx.jda.shardManager?.shards?.filter { it.status != JDA.Status.CONNECTED }?.takeIf { it.isNotEmpty() }
+            ?: return ctx.send("There are no dead shards.")
+
+        deadShards.forEach { it.shardManager?.restart(it.shardInfo.shardId) }
+        ctx.send("Queued **${deadShards.size}** shards for reboot.")
+    }
+
+    @SubCommand(description = "Restart all shards.")
+    fun all(ctx: Context) {
+        ctx.send("Rebooting all **${ctx.jda.shardInfo.shardTotal}** shards...")
+        ctx.jda.shardManager?.restart()
+    }
 }
