e927c30b963b3cb126ea3acd55c1ad0c9768a5b2
Farami <webmaster@farami.de>
2020-05-18 17:16:34 +0200

    Add descriptions

--- src/main/kotlin/gg/octave/bot/commands/music/Playlist.kt
+++ src/main/kotlin/gg/octave/bot/commands/music/Playlist.kt
@@ -13,10 +13,10 @@ import me.devoxin.flight.api.entities.Cog
 class Playlist : Cog {
     @Command(aliases = ["pl"], description = "Save or load a playlist.")
     fun playlist(ctx: Context) {
-        ctx.send("Try load or save.")
+        ctx.send("Try load, save, list or delete.")
     }
 
-    @SubCommand()
+    @SubCommand(description = "Lists all your playlists.")
     fun list(ctx: Context) {
         val playlists = ctx.db.getPlaylists(ctx.author.id)?.toList()
             ?: emptyList()
@@ -36,19 +36,19 @@ class Playlist : Cog {
         }
     }
 
-    @SubCommand(aliases = ["l"])
+    @SubCommand(aliases = ["l"], description = "Load a playlist.")
     fun load(ctx: Context, name: String) {
-        val formattedName = "${ctx.author.id}:$name"
-        val playlist = ctx.db.getPlaylist(formattedName)?.toLavaPlaylist()
+        val id = "${ctx.author.id}:$name"
+        val playlist = ctx.db.getPlaylist(id)?.toLavaPlaylist()
             ?: return ctx.send("Playlist with the name **$name** could not be found.")
 
         val trackContext = TrackContext(ctx.author.idLong, ctx.textChannel!!.idLong)
-        val resultHandler = ctx.manager.MusicManagerAudioLoadResultHandler(ctx, formattedName, trackContext, isNext = false)
+        val resultHandler = ctx.manager.MusicManagerAudioLoadResultHandler(ctx, id, trackContext, isNext = false)
 
         resultHandler.playlistLoaded(playlist)
     }
 
-    @SubCommand(aliases = ["s"])
+    @SubCommand(aliases = ["s"], description = "Save a playlist.")
     fun save(ctx: Context, name: String) {
         val queue = ctx.manager.scheduler.queue.toList()
         if (queue.count() == 0) {
@@ -63,7 +63,7 @@ class Playlist : Cog {
         ctx.send("Playlist saved.")
     }
 
-    @SubCommand(aliases = ["d"])
+    @SubCommand(aliases = ["d"], description = "Delete a playlist.")
     fun delete(ctx: Context, name: String) {
         ctx.db.getPlaylist("${ctx.author.id}:$name")?.delete()
             ?: ctx.send("Playlist not found.")
