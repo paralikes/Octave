b567e7a92ed01195faf9345638f50f55c43a1511
Marilena Louis <marilena.louis@gmail.com>
2020-05-18 17:21:50 +0200

    Count automatically added reaction as vote

--- src/main/kotlin/gg/octave/bot/commands/music/VoteSkip.kt
+++ src/main/kotlin/gg/octave/bot/commands/music/VoteSkip.kt
@@ -95,7 +95,7 @@ class VoteSkip : MusicCog {
                     }.build()
                 ).submitAfter(voteSkipDuration, TimeUnit.MILLISECONDS)
             }.thenAccept { m ->
-                val skip = m.reactions.firstOrNull { it.reactionEmote.name == "👍" }?.count?.minus(1) ?: 0
+                val skip = m.reactions.firstOrNull { it.reactionEmote.name == "👍" }?.count ?: 0
 
                 ctx.send {
                     setTitle("Vote Skip")
